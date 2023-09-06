9024404168920289e2c149eb9a8c5ffecdec0e44
Irabor Kate Chika <iraborkate29@gmail.com>
2023-09-06 02:03:16 +0100

    objects on javascripts

--- javascript-warm_up/103-object_fct.js
+++ javascript-warm_up/103-object_fct.js
@@ -3,12 +3,12 @@
 const myObject =  {
     type: 'object',
     value: 12,
-    incr: function () {
-        this.value++;
-    },
 };
-
 console.log(myObject);
+ 
+myObject.incr = function () {
+    this.value++;
+};
 
 myObject.incr();
 console.log(myObject);
--- javascript-warm_up/12-object.js
+++ javascript-warm_up/12-object.js
@@ -2,7 +2,7 @@ let object = {
     type: Object,
     value: 12
 };
+console.log(object)
 
 object.value = 89;
-
 console.log(object)
