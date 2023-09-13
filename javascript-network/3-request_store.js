5e7de872816457bfa60b625aa78a92f1b0a106ba
Irabor Kate Chika <iraborkate29@gmail.com>
2023-09-13 19:33:19 +0100

    javascrit

--- javascript-network/3-request_store.js
+++ javascript-network/3-request_store.js
@@ -9,8 +9,9 @@ const filepath = process.argv[3];
 req(url, (error, response, body) => {
     if (!error && response.statusCode === 200) {
         fs.writeFile(filepath, body, { encoding: 'utf-8' }, (err) => {
-            if (err) throw err;
-            console.log(`File saved to ${filepath}`);
+            if (err) {
+            console.error(`Error writing to file: ${err}`);
+            }
         });
     } else {
         console.error(`Error fetching URL: ${error}`);
