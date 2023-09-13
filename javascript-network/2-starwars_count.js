954b2fbc41c7c689a180ad23a8e6207c7bc8f9b6
Irabor Kate Chika <iraborkate29@gmail.com>
2023-09-13 19:16:34 +0100

    javascript

--- javascript-network/2-starwars_count.js
+++ javascript-network/2-starwars_count.js
@@ -2,20 +2,27 @@
 // using a module request to make common requests
 const req = require("request");
 
-const url = process.argv[2];
+const url = `${process.argv[2]}`;
 
-const characterId = 18;
 
 // passing the json string into a object for easy access
 // becomes easy to request for the header
 req(url, (error, response, body) => {
     if (error) {
-        console.error('Error');
+        return console.error(error);
     }
-    const filmsData = JSON.parse(body);
-    const filmsWithWedgeAntilles = filmsData.filter(film => film.characters.includes(`https://swapi-api.alx-tools.com/api/people/${characterId}/`));
-    
-    console.log(filmsWithWedgeAntilles.length);
+    const data = JSON.parse(body);
+    const films = data.results;
+    let count = 0;
+    const characterId = "18";
+    const character =`https://swapi-api.alx-tools.com/api/people/${characterId}/`;
+
+    for (const film of films) {
+        if (film.characters.includes(character)) {
+            count += 1;
+        }
+    }
+     console.log(count);
 });
 
 
