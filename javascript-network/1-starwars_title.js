828b113c499e0998245815458332dee9d2381824
Irabor Kate Chika <iraborkate29@gmail.com>
2023-09-13 07:49:33 +0100

    javascript

--- javascript-network/1-starwars_title.js
+++ javascript-network/1-starwars_title.js
@@ -1,19 +1,19 @@
 #!/usr/bin/node
 // using a module request to make common requests
 const req = require('request');
-
 const id = process.argv[2];
-
 const url = `http://swapi-api.alx-tools.com/api/films/${id}`;
+
 // passing the json string into a object for easy access
 // becomes easy to request for the header
 req(url, (error, response, body) => {
-    if (!error && response.statusCode === 200) {
-        const info = JSON.parse(body);
-        const header = info.title;
-        console.log(header);
-    } else {
-        console.error('Error fetching movie title');
+    if (error) {
+        return console.error(error);
     }
+    const obj = JSON.parse(body);
+    console.log(obj.title);
+
 });
+    
+    
 
