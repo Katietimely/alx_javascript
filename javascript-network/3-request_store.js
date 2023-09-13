97705dbf73296e6f735ba50f5aeb60cae8666cb7
Irabor Kate Chika <iraborkate29@gmail.com>
2023-09-13 19:17:14 +0100

    javascr

---
+++ javascript-network/3-request_store.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+// using a module request to make common requests
+const req = require("request");
+const fs = require("fs");
+
+const url = process.argv[2];
+const filepath = process.argv[3];
+
+req(url, (error, response, body) => {
+    if (!error && response.statusCode === 200) {
+        fs.writeFile(filepath, body, { encoding: 'utf-8' }, (err) => {
+            if (err) throw err;
+            console.log(`File saved to ${filePath}`);
+        });
+    } else {
+        console.error(`Error fetching URL: ${error}`);
+    }
+});
